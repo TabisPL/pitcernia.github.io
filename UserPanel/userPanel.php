a5d236bc8383a20157301b90db29578f041f533f
TabisPL <tabiszewski.16@interia.pl>
2025-01-22 21:34:12 +0100

    logo image

--- UserPanel/userPanel.php
+++ UserPanel/userPanel.php
@@ -35,11 +35,11 @@ $baza = mysqli_connect($serwer, $uzytkownik, $haslo, $baza_danych);
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="placeholder.jpg"></use></svg>
+        <img src="placeholder.png" alt="Logo" class="Logo">
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">HOME</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">HOME</a></li>
           <li><a href="#" class="nav-link px-2 text-white">MENU</a></li>
           <li><a href="#" class="nav-link px-2 text-white">KOSZYK</a></li>
           <li><a href="#" class="nav-link px-2 text-white">MOJE KONTO</a></li>
