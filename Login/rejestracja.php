28aa6e76bb81d05f7d9573c71b405076be34915a
Mimamump <milosz.polak.priv@gmail.com>
2025-01-23 16:08:01 +0100

    Update rejestracja.php

--- Login/rejestracja.php
+++ Login/rejestracja.php
@@ -97,7 +97,7 @@ mysqli_close($conn);
 </head>
 <body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <!-- NAGŁÓWEK -->
+    <!-- NAVBAR -->
     <header class="p-3 text">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
