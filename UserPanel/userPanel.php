20b5b0d4d6390798415bf8ca5b7ad635199021de
TabisPL <tabiszewski.16@interia.pl>
2025-01-23 22:04:52 +0100

    bootstrap act

--- UserPanel/userPanel.php
+++ UserPanel/userPanel.php
@@ -28,26 +28,24 @@ $baza = mysqli_connect($serwer, $uzytkownik, $haslo, $baza_danych);
     <link rel="stylesheet" href="UserPanel.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark text-white">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <!-- NAGŁÓWEK -->
-<header class="p-3 text">
+<header class="p-3 bg-orange text-white">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-        <img src="placeholder.png" alt="Logo" class="Logo">
-        </a>
+        <img src="https://i.imgur.com/hUa9V6E.png" alt="Logo" class="Logo" style="width: 100px; height: auto;"></a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-white">HOME</a></li>
           <li><a href="#" class="nav-link px-2 text-white">MENU</a></li>
           <li><a href="#" class="nav-link px-2 text-white">KOSZYK</a></li>
           <li><a href="#" class="nav-link px-2 text-white">MOJE KONTO</a></li>
         </ul>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2"><a href="login\test2-login.php">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+        <button type="button" class="btn btn-outline-light"><a href="login.php">Login</button></a>
+        <button type="button" class="btn btn-warning"><a href="rejstracja.php">Sign-up</button></a>
         </div>
       </div>
     </div>
