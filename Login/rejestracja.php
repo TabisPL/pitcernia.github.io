c5dedd35628d8d22cda36c2d8c8fe354e94de7af
Mimamump <milosz.polak.priv@gmail.com>
2025-01-23 16:03:38 +0100

    Update rejestracja.php

--- Login/rejestracja.php
+++ Login/rejestracja.php
@@ -1,4 +1,10 @@
 <?php
+
+session_start();
+
+$czyzalogowany = isset($_SESSION['UzytkownikID']);
+
+
 // Dane do połączenia z bazą danych
 $serwer = 'localhost';
 $baza_danych = 'pizza3test';
@@ -87,21 +93,28 @@ mysqli_close($conn);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rejestracja</title>
     <link rel="stylesheet" href="login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- NAGŁÓWEK -->
-    <header>
-        <div class="banner">
-            <div class="logo">PIZZA TEST</div>
-            <nav>
-                <ul class="menu">
-                    <li><a href="#">MENU</a></li>
-                    <li><a href="#">KOSZYK</a></li>
-                    <li><a href="mojekonto.php">MOJE KONTO</a></li>
-                    <li><a href="login.php" class="login-button">Login</a></li>
-                </ul>
-            </nav>
-        </div>
+    <header class="p-3 text">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <img src="https://i.imgur.com/hUa9V6E.png" alt="Logo" class="Logo" style="width: 100px; height: auto;">
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="#" class="nav-link px-2 text-white">MENU</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">KOSZYK</a></li>
+          <li><a href="mojekonto.php" class="nav-link px-2 text-white">MOJE KONTO</a></li>
+        </ul>
+        <!-- If na sprawdzenie czy wyswietlamy guziki -->
+    <?php if (!$czyzalogowany): ?>
+        <a href="login.php" class="btn btn-outline-light me-2">Login</a>
+        <a href="rejestracja.php" class="btn btn-warning">Sign-up</a>
+    <?php endif; ?>
     </header>
     <main>
         <section class="form-container">
