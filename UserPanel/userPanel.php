b0dc175e939cd9c2f972fd6ba87c3b09e9231235
TabisPL <tabiszewski.16@interia.pl>
2025-01-31 12:07:37 +0100

    tabela

--- UserPanel/userPanel.php
+++ UserPanel/userPanel.php
@@ -18,17 +18,18 @@ if (isset($_SESSION['UzytkownikID'])) {
 
 // Funkcja tworzy tabelę do wyświetlania zamówień i pobiera je z bazy danych
 function get_orders($baza, $logged_user) {
-  $sql = "SELECT Ilosc, Suma, UzytkownikID, KwotaCalkowita, Status, zamowienia.DataUtworzenia, DataAktualizacji, szczegolyzamowienia.PizzaID, Nazwa, Cena, Rozmiar FROM `szczegolyzamowienia` 
-  JOIN zamowienia ON szczegolyzamowienia.ZamowienieID = zamowienia.ZamowienieID JOIN pizze ON szczegolyzamowienia.PizzaID = pizze.PizzaID WHERE UzytkownikID = '$logged_user';";
+  $sql = "SELECT zamowienia.KwotaCalkowita, zamowienia.Status, zamowienia.DataUtworzenia, zamowienia.DataAktualizacji, szczegolyzamowienia.Ilosc, pizze.Nazwa, pizze.Rozmiar FROM `zamowienia`
+  JOIN szczegolyzamowienia ON szczegolyzamowienia.ZamowienieID = zamowienia.ZamowienieID JOIN pizze ON pizze.PizzaID = szczegolyzamowienia.PizzaID
+  WHERE zamowienia.UzytkownikID = '$logged_user';";
   $orders = mysqli_query($baza, $sql);
   // Tworzenie tabeli dla każdego zamówienia osobno
   foreach($orders as $order) {
-    echo "<table class='table table-bordered'>";
-    echo "<tr><td>Nazwa:<td/><td>".$order["Nazwa"]."</td></tr>";
-    echo "<tr><td>Rozmiar:<td/><td>".$order["Rozmiar"]."</td></tr>";
-    echo "<tr><td>Cena:<td/><td>".$order["Cena"]."</td><td>Kwota całkowita:</td></tr>";
-    echo "<tr><td>Ilość:<td/><td>".$order["Ilosc"]."</td><td>".$order["KwotaCalkowita"]."</td></tr>";
-    echo "<tr><td>Status:<td/><td>".$order["Status"]."</td></tr>";
+    echo "<table class='table table-bordered table-rounded'>";
+    echo "<tr><td>Data utworzenia:</td><td>".$order["DataUtworzenia"]."</td><td>Ostatnia aktualizacja:</td><td>".$order["DataAktualizacji"]."</td></tr>";
+    echo "<tr><td>Pizza:</td><td>".$order["Nazwa"]."</td><td>Status:</td><td>".$order["Status"]."</td></tr>";
+    echo "<tr><td>Rozmiar:</td><td>".$order["Rozmiar"]."</td></tr>";
+    echo "<tr><td>Ilość:</td><td>".$order["Ilosc"]."</td></tr>";
+    echo "<tr><td>Cena:</td><td>".$order["KwotaCalkowita"]."</td></tr>";
     echo "</table>";
   }
 }
@@ -52,7 +53,7 @@ function get_orders($baza, $logged_user) {
       <a class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
       <img src="https://i.imgur.com/hUa9V6E.png" alt="Logo" class="Logo" style="width: 100px; height: auto;"></a>
       <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-2 text-white">MENU</a></li>
+        <li><a href="../menu.php" class="nav-link px-2 text-white">MENU</a></li>
         <li><a href="#" class="nav-link px-2 text-white">KOSZYK</a></li>
         <li><a href="../UserPanel/userPanel.php" class="nav-link px-2 text-white">MOJE KONTO</a></li>
       </ul>
@@ -67,7 +68,13 @@ function get_orders($baza, $logged_user) {
 </header>
 <main>
   <div class="col-8">
-    <?php get_orders($baza, $logged_user); ?>
+    <?php 
+    if (isset($_SESSION['UzytkownikID'])) {
+      get_orders($baza, $logged_user);
+    }
+    else {
+      echo  "Użytkownik nie zalogowany!";
+    }?>
   </div>
 </main>
 
