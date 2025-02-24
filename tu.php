cfe092ec0bb8eadfd6551c6979b09cf2aeffa5e7
wojtos102aa <98663064+wojtos102aa@users.noreply.github.com>
2025-02-24 13:02:57 +0100

    Add files via upload

---
+++ tu.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>test</title>
+  <link rel="stylesheet" href="tu.css" />
+ 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+</head>
+<body>
+  <div class="container">
+    <nav>
+      <ul>
+        <a href="#" class="logo">
+          <img src="placeholder.jpg" alt="">
+          <span class="nav-item">DashBoard</span>
+        </a>
+        <a href="#">
+          <i class="fas fa-home"></i>
+          <span class="nav-item">Home</span>
+        </a>
+        <a href="">
+          <i class="fas fa-user"></i>
+          <span class="nav-item">Profile</span>
+        </a>
+        <a href="">
+          <i class="fas fa-wallet"></i>
+          <span class="nav-item">Wallet</span>
+        </a>
+        
+        <a href="">
+          <i class="fas fa-tasks"></i>
+          <span class="nav-item">Tasks</span>
+        </a>
+        <a href="">
+          <i class="fas fa-cog"></i>
+          <span class="nav-item">Settings</span>
+        </a>
+        <a href="">
+          <i class="fas fa-question-circle"></i>
+          <span class="nav-item">Help</span>
+        </a>
+        <a href="" class="logout">
+          <i class="fas fa-sign-out-alt"></i>
+          <span class="nav-item">Log out</span>
+        </a>
+      </ul>
+    </nav>
+    <section class="main">
+      <div class="main-top">
+        <h1>Pizza</h1>
+        
+      </div>
+      <div class="main-skills">
+        <div class="card">
+          
+          <h3>zamówienia</h3>
+          <p>pizza</p>
+          <button>sprawdz</button>
+        </div>
+        <div class="card">
+          
+          <h3>zrealizowane</h3>
+          <p>pizza</p>
+          <button>sprawdz</button>
+        </div>
+        <div class="card">
+          
+          <h3>anulowane</h3>
+          <p>pizza</p>
+          <button>sprawdz</button>
+        </div>
+        <div class="card">
+          
+          <h3>pizze</h3>
+          <p>pizza</p>
+          <button>pizza</button>
+        </div>
+      </div>
+      
+      </section>
+    </section>
+  </div>
+</body>
+</html></span>
