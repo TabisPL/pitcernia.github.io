6af4f070cdbaf7557bc5b9b5c7ee2bb13c34d011
Tadeusz45 <tadeuszmuca1@gmail.com>
2025-03-02 19:44:32 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,6 +1,5 @@
 <?php
 session_start(); // Start sesji
-
 $czyzalogowany = isset($_SESSION['UzytkownikID']);
 
 // Dane do połączenia z bazą danych
@@ -22,77 +21,119 @@ $baza = mysqli_connect($serwer, $uzytkownik, $haslo, $baza_danych);
     <title>Logowanie</title>
     <link rel="stylesheet" href="menu.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/navbar.css">
 </head>
 <body>
+<?php include '/navbar.php'; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <!-- NAGŁÓWEK -->
-    <header class="p-3 text">
+<?php include_once ("menu.php"); ?>
+    <!-- Formularz wybory pizzy po cenie, wielkości -->
+    <div class="formularz">
+      <form method="post" >  
+          <div class="input-group col-auto">
+            <span class="input-group-text">Cena od do</span>
+            <input type="number" aria-label="cena_min" class="form-control" name="cena_min" id="cena_min" value="0">
+            <input type="number" aria-label="cena_max" class="form-control" name="cena_max" id="cena_max" value="100">
+          </div>
+
+        <div class="input-group mb-3">
+          <label class="input-group-text" for="rozmiar">Rozmiar</label>
+          <select class="form-select" id="rozmiar" name="rozmiar">
+            <option value="%" selected>Wybierz rozmiar...</option>
+            <option value="Mala">Mała</option>
+            <option value="Srednia">Średnia</option>
+            <option value="Duza">Duża</option>
+          </select>
+        </div> 
+
+        <?php
+        // Tworzenie formularza do wyboru składników
+
+          // $sql_skladniki = 'SELECT * FROM skladniki';
+          // $skladniki = $baza->query($sql_skladniki);
+          // var_dump($skladniki);
+          // foreach ($skladniki as $s ){
+          //   $skladnik = $s["NazwaSkladnika"];
+          //   // wybrac ID skladnika zamiast nazwy
+          //   $skladnik_id = $s["SkladnikID"];
+          //   echo('
+          //   <div class="form-check">
+          //     <input class="form-check-input" type="checkbox" value="" id="'. $skladnik .'">
+          //     <label class="form-check-label" for="'. $skladnik .'">
+          //       '. $skladnik .'
+          //     </label>
+          //   </div>
+          //   ');
+          // }
+        ?>
+
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary">Wyszukaj</button> 
+        </div>
+      </form>
+    </div>
+
+
     <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-        <img src="https://i.imgur.com/hUa9V6E.png" alt="Logo" class="Logo" style="width: 100px; height: auto;">
-        </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="menu.php" class="nav-link px-2 text-white">MENU</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">KOSZYK</a></li>
-          <li><a href="mojekonto.php" class="nav-link px-2 text-white">MOJE KONTO</a></li>
-        </ul>
-
-       <!-- If na sprawdzenie czy wyswietlamy guziki --> 
-    <?php if (!$czyzalogowany): ?>
-        <a href="login.php" class="btn btn-outline-light me-2">Login</a>
-        <a href="rejestracja.php" class="btn btn-warning">Sign-up</a>
-        <?php endif; ?>
-    </header>
-
-
-
-    <div class="container">
-
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-        
-
-        
       <?php 
 
-        $sql = "SELECT * FROM Pizze";
+        if (isset($_POST['cena_min'])){
+          $cena_min = $_POST['cena_min'];
+        } else {
+          $cena_min = 0;
+        }
+        if (isset($_POST['cena_max'])){
+          $cena_max = $_POST['cena_max'];
+        } else {
+          $cena_max = 110;
+        }
+        if (isset($_POST['rozmiar'])){
+          $rozmiar = $_POST['rozmiar'];
+        } else {
+          $rozmiar = "%";
+        }
+
+        //SQL na składniki 
+        // SELECT * FROM `PizzaSkladniki` JOIN Pizze ON Pizze.PizzaID=PizzaSkladniki.PizzaID JOIN Skladniki ON Skladniki.SkladnikID=PizzaSkladniki.SkladnikID  WHERE Skladniki.Nazwa LIKE 'koń'
+        // SELECT * FROM Pizze JOIN PizzaSkladniki ON Pizze.PizzaID=PizzaSkladniki.PizzaID JOIN Skladniki ON Skladniki.SkladnikID=PizzaSkladniki.SkladnikID WHERE Cena BETWEEN '.$cena_min.' AND '.$cena_max.' AND rozmiar LIKE "'. $rozmiar.'"';
+        $sql = 'SELECT * FROM Pizze WHERE Cena BETWEEN '.$cena_min.' AND '.$cena_max.' AND rozmiar LIKE "'. $rozmiar.'"';
+        // var_dump($sql);
         $result = $baza->query($sql);
-        var_dump($result);
+        // var_dump($result);
         foreach($result as $p) {
           $nazwa = $p["Nazwa"];
-          echo '<div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          $image = $p["ObrazekURL"];
+          $opis = $p["Opis"];
+          $cena=$p["Cena"];
+          $rozmiar = $p["Rozmiar"];
+          echo '<div class="card-deck">';
+          echo '
+          <div class="col">
+          <div class="card shadow-sm ">
             <div class="card-body">
-              <p class="card-text">' . $nazwa . '</p>
-              <div class="d-flex justify-content-between align-items-center">
-              </div>
+              <img class="card-img-top" src="'. $image .'" alt="image">
+              <h3 class="card-title">' . $nazwa . '</h3>
+              <p class="card-text">' . $opis . '</p>
+            
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Cena: ' . $cena . ' zł</li>
+              <li class="list-group-item">Rozmiar: '.$rozmiar.'</li>
+            </ul>
+            <div class="card-body">
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="card-link">Kup</a>
             </div>
+          </div>
           </div>
         </div>
         ';
-        var_dump($p);
+        echo '</div>';
+        // var_dump($p);
         }
       ?>
-        
-        
+
       </div>
     </div>
+    
 </body>
-
-<div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">' . $nazwa . '</p>
-              <div class="d-flex justify-content-between align-items-center">
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-
-
 </html>
